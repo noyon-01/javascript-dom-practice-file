2c8fa59509ed1304513ff259eb5a81bf47a20b53
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-02-15 22:32:15 +0600

    javascript dom tagName selector practice is completed

--- javascript-dom-selector/script.js
+++ javascript-dom-selector/script.js
@@ -3,7 +3,43 @@ const headingElement = document.getElementsByTagName("h1");
 headingElement[0].style.color = "red";
 headingElement[0].style.fontSize = "5rem";
 
-// const paragraphElement
+const paragraphElement = document.getElementsByTagName("p");
+paragraphElement[0].style.color = "aqua";
+paragraphElement[0].style.textAlign = "center";
+paragraphElement[0].style.fontSize = "1.25rem";
+paragraphElement[0].style.marginTop = "20px";
+
+const ulElement = document.getElementsByTagName("ul");
+ulElement[0].style.display = "flex";
+ulElement[0].style.justifyContent = "center";
+ulElement[0].style.alignItems = "center";
+ulElement[0].style.gap = "50px";
+ulElement[0].style.backgroundColor = "tomato";
+ulElement[0].style.margin = "50px 170px";
+ulElement[0].style.padding = "50px 100px 70px";
+ulElement[0].style.borderRadius = "10px";
+
+const liElement = document.getElementsByTagName("li");
+for (const li of liElement) {
+  li.style.listStyle = "none";
+  li.style.backgroundColor = "white";
+  li.style.padding = "10px 25px";
+  li.style.marginTop = "20px";
+  li.style.borderRadius = "10px";
+}
+
+const anqorElement = document.getElementsByTagName("a");
+for (const a of anqorElement) {
+  a.style.textDecoration = "none";
+  a.style.color = "black";
+  a.style.fontWeight = "bold";
+}
+
+const buttonElement = document.getElementsByTagName("button");
+buttonElement[0].style.fontSize = "1.5rem";
+buttonElement[0].style.fontWeight = "bold";
+buttonElement[0].style.padding = "10px 25px";
+buttonElement[0].style.borderRadius = "10px";
 
 // !JavaScript Dom ID Selector is Starts Here!//
 
