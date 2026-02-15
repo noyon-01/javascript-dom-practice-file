83e19dc4e2dc8862d16fb551c94047782072ef7f
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-02-15 21:57:26 +0600

    javascript dom tagName selector is added

---
+++ javascript-dom-selector/script.js
@@ -0,0 +1,14 @@
+// !JavaScript Dom TagName Selector is Starts Here!//
+const headingElement = document.getElementsByTagName("h1");
+headingElement[0].style.color = "red";
+headingElement[0].style.fontSize = "5rem";
+
+// const paragraphElement
+
+// !JavaScript Dom ID Selector is Starts Here!//
+
+// !JavaScript Dom Class Selector is Starts Here!//
+
+// !JavaScript Dom Query Selector is Starts Here!//
+
+// !JavaScript Dom Query Selector All is Starts Here!//
