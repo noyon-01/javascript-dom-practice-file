b1dfc3d33f4e697c17a46f440e092df3093de0fa
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-02-16 15:03:36 +0600

    javascript dom classlist add and remove practice is added

---
+++ javascript-dom-text-change/script.js
@@ -0,0 +1,17 @@
+// !JavaScript Dom Text Changeing Starts Here! //
+// const heading_01 = document.getElementById("heading_01");
+// heading_01.innerText = "Hello JavaScript Document Object Model Dom How Are You!";
+
+// const heading_02 = document.getElementsByClassName("heading_02");
+// heading_02[0].innerText = "My Hobby is Programming and Gardening!";
+
+// const heading_03 = document.querySelector(".heading_03");
+// heading_03.innerText = "I Love to Coding!";
+
+// !JavaScript Dom Add and Remove Classlist Starts Here! //
+// const navber_box = document.querySelector("#navber");
+// navber_box.classList.add("navber_box");
+
+// const navber_list = document.querySelector("#navber_list");
+// navber_list.classList.remove("navber-list");
+// navber_list.classList.add("navber-list");
