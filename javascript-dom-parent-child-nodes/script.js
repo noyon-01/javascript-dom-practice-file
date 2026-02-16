b926596b281c8443316d8d12141c0df4c7cb5e3e
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-02-16 15:48:56 +0600

    javascript dom parent and child nodes, create element is added

---
+++ javascript-dom-parent-child-nodes/script.js
@@ -0,0 +1,13 @@
+// const main_section = document.querySelector(".main_section");
+// const childs = main_section.childNodes;
+// console.log(childs);
+
+// const unorderList = document.getElementById("unorderList");
+// console.log(unorderList.childNodes[0])
+// console.log(unorderList.childNodes[1])
+// console.log(unorderList.childNodes[2])
+// console.log(unorderList.childNodes[3])
+
+// const unorderList = document.getElementById("unorderList");
+// const body = unorderList.parentNode.parentNode.parentNode.parentNode;
+// body.style.backgroundColor = "red";
