d28effb6b674838de0da715e4eb68b4772877abd
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-02-17 20:24:47 +0600

    javascrip dom addeventlister click function is added

--- javascript-dom-onclink-event/script.js
+++ javascript-dom-onclink-event/script.js
@@ -28,3 +28,36 @@ makeRedBtn.onclick = makeRed;
 function makeRed() {
   document.body.style.backgroundColor = "red";
 }
+
+const makeTomato = document.getElementById("make_tomato");
+makeTomato.addEventListener("click", function makeTomato() {
+  document.body.style.backgroundColor = "tomato";
+});
+
+const makeAqua = document.getElementById("make_aqua");
+makeAqua.addEventListener("click", function () {
+  document.body.style.backgroundColor = "aqua";
+});
+
+const paragraphText = document.getElementById("paragraph_text");
+const changeTextBtn = document.getElementById("text_change_btn");
+changeTextBtn.addEventListener("click", function () {
+  paragraphText.innerText = "My Name is Shahadat Hosen Noyon";
+});
+
+const paragraph_text1 = document.getElementById("paragraph_text1");
+
+const name_input = document.getElementById("name_input");
+console.log();
+
+const name_add_btn = document.getElementById("name_add_btn");
+
+name_add_btn.addEventListener("click", function () {
+  const input_value = name_input.value;
+
+  const newParagraph = document.createElement("p");
+  newParagraph.innerText = input_value;
+
+  paragraph_text1.appendChild(newParagraph);
+  name_input.value = "";
+});
