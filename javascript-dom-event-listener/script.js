f2c0854bf39cd773538a776d85e5aba39e2e5dde
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-02-18 22:19:25 +0600

    javascript event listener is updated

--- javascript-dom-event-listener/script.js
+++ javascript-dom-event-listener/script.js
@@ -1,9 +1,5 @@
 // !JavaScript Dom Adeventlistener Click Function Starts Here! //
 document.getElementById("change_heading_btn").addEventListener("click", function () {
-    const input_element = document.getElementById("input_fild");
-    const input_value = input_element.value;
-    const newHeading = document.createElement("h1");
-    newHeading.innerText = input_value;
     const heading1 = document.getElementById("heading1");
-    heading1.appendChild(newHeading);
+    heading1.innerText = "I Love To Programming and I Also Love To Writting Code";
   });
